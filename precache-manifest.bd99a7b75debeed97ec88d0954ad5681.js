5261b9f7b10969c47c9c5d52904413141fdd0d7e
Dayvid Montovanelli Moreira <dayvidmoreira7@gmail.com>
2019-09-22 22:40:08 -0300

    initial commit

---
+++ precache-manifest.bd99a7b75debeed97ec88d0954ad5681.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38e6e9bd7d0c16d3519e2c148d54a2ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a223170500ec3f40e7bd",
+    "url": "/static/css/main.e8c24495.chunk.css"
+  },
+  {
+    "revision": "14bb9ee80d5af0253e6d",
+    "url": "/static/js/2.a5b82793.chunk.js"
+  },
+  {
+    "revision": "a223170500ec3f40e7bd",
+    "url": "/static/js/main.7cc83a52.chunk.js"
+  },
+  {
+    "revision": "9f3b6f5d85035e0b1d9d",
+    "url": "/static/js/runtime-main.a45aa8df.js"
+  }
+]);
